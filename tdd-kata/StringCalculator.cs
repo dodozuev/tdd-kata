763bb986c72d206b0340c4f4b1bd8b2d5a1e3369
d.zuev <d.zuev@dodopizza.com>
2019-11-08 10:30:11 +0300

    WhenSingleChar_ShouldBeCharParsed

--- tdd-kata/StringCalculator.cs
+++ tdd-kata/StringCalculator.cs
@@ -4,7 +4,10 @@
     {
         public int Add(string input)
         {
+            if(string.IsNullOrEmpty(input))
             return 0;
+
+            return int.Parse(input);
         }
     }
 }
